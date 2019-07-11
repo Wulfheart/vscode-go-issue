94bd27806a50573860804cdcf5bb327e4907611e
Alexander Wulf <dev@alexfwulf.de>
2019-07-11 17:13:36 +0200

    Little test

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
-	"github.com/kellydunn/golang-geo"
 	"fmt"
+
+	geo "github.com/kellydunn/golang-geo"
 )
 
-func main(){
-	fmt.Println(geo.NewPoint(0,0))
+func main() {
+	poly := []*geo.Point{
+		geo.NewPoint(0, 0), geo.NewPoint(0, 1), geo.NewPoint(1, 1), geo.NewPoint(1, 0),
+	}
+	gon := geo.NewPolygon(poly)
+	fmt.Println("x:0\ty:0\t", "Expecting: true\t", "Got:", gon.Contains(geo.NewPoint(0, 0)))
+	fmt.Println("x:0.5\ty:0\t", "Expecting: true\t", "Got:", gon.Contains(geo.NewPoint(0.5, 0)))
+	fmt.Println("x:0.5\ty:0.7\t", "Expecting: true\t", "Got:", gon.Contains(geo.NewPoint(0.5, 0.7)))
+	fmt.Println("x:2\ty:8\t", "Expecting: false\t", "Got:", gon.Contains(geo.NewPoint(2, 8)))
+
 }
